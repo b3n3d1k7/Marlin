22dc98195ab165a48e941994312650f979a2472a
bene <benedikt.bleimhofer@gmail.com>
2020-04-26 15:34:53 +0200

    fix steps per mm + enable linear advance

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -610,7 +610,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 108, 108, 108, 419 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 110, 110, 110, 419 }
 
 /**
  * Default Max Feed Rate (mm/s)
