346782efb9b7e409fbadfd35947177b8ab3b2a1f
Benedikt Bleimhofer <benedikt.bleimhofer@wirelane.com>
2021-04-18 15:51:20 +0200

    enable babystepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -996,7 +996,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 37, 16, -2.40 }
+#define NOZZLE_TO_PROBE_OFFSET { 37, 16, -2.20 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
