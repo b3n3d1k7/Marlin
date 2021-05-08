2207cef6c24d864354db725907531cdd30a6ed66
Benedikt Bleimhofer <benedikt.bleimhofer@wirelane.com>
2021-05-08 19:27:39 +0200

    adjust probe offset, margin and x-offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1057,11 +1057,11 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 37, 8, -1.0 }
+#define NOZZLE_TO_PROBE_OFFSET { 37, 8, -5.85 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define PROBING_MARGIN 10
+#define PROBING_MARGIN 20
 
 // X and Y axis travel speed (mm/min) between probes
 #define XY_PROBE_FEEDRATE (133*60)
@@ -1239,7 +1239,7 @@
 #define Y_BED_SIZE 200
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS -27
+#define X_MIN_POS -22
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
